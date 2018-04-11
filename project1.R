ac9182a2ccb3453ca85cd7543ad23f9c839f67f7
jamesmahler2 <jmahler@andrew.cmu.edu>
2018-04-10 22:58:19 -0400

    done

--- project1.R
+++ project1.R
@@ -3,10 +3,15 @@ pitch_data <- read.csv("savant_data.csv")
 pitch_data <- pitch_data[pitch_data$pitch_type %in% c("CH", "CU", "FC", "FF", "FT", "SI", "SL"),]
 pitch_data$launch_speed <- as.numeric(as.character(pitch_data$launch_speed))
 pitch_data$release_speed <- as.numeric(as.character(pitch_data$release_speed))
-View(pitch_data)
+pitch_data$effective_speed <- as.numeric(as.character(pitch_data$effective_speed))
+pitch_data$release_spin_rate <- as.numeric(as.character(pitch_data$release_spin_rate))
+pitch_data$pfx_x <- as.numeric(as.character(pitch_data$pfx_x))
+pitch_data$pfx_z <- as.numeric(as.character(pitch_data$pfx_z))
+
+
+
 pd_right <- pitch_data[pitch_data$p_throws=="R",]
 pd_left <- pitch_data[pitch_data$p_throws=="L",]
-
 #########################################
 #THEME
 Static_Poster_Theme <- theme_bw() + 
@@ -31,10 +36,14 @@ Static_Poster_Theme <- theme_bw() +
 
 #########################################
 ## Looking at pitch selection
-ggplot(pitch_data, aes(x = pitch_type)) + Static_Poster_Theme +  geom_bar(aes(y = (..count..)/sum(..count..))) +
-  labs(title = "Pitch Selection", subtitle = "Proportion of pitches selected with 2 outs",
-       x = "Pitch Type", y = "Proportion")
+ggplot(pitch_data, aes(x = pitch_type, fill = pitch_type)) + Static_Poster_Theme + 
+  geom_bar(aes(y = (..count..)/sum(..count..))) +
+  scale_fill_brewer(type = "qual", palette = "Dark2") +
+  labs(title = "4 Seam is Most Common Pitch Selected",
+       subtitle = "About 50% fastballs, 20% sliders, 10% curve/changeup",
+       x = "Pitch Type", y = "Proportion", fill = "Pitch Type")
 ggsave("pitch_selection.png", dpi = 300)
+
 #Pitch selection based on handedness
 ggplot(pd_right, aes(x = pitch_type)) +
   geom_bar(aes(y = (..count..)/sum(..count..)))
@@ -59,8 +68,9 @@ type_velo <- type_velo %>% group_by(pitch_type) %>%
 #type_velo <- type_velo[type_velo$num_pitches>100,] #Take only common pitches
 View(type_velo)
 ggplot(type_velo, aes(x = pitch_speed, y = launch_velo)) +
-  geom_smooth(method = "lm", se = FALSE) + geom_text(aes(label = pitch_type)) + Static_Poster_Theme +
-  labs(title = "Exit Velocity off the Bat for each Pitch Type",
+  geom_smooth(method = "lm", se = FALSE, color = "red4") +
+  geom_text(aes(label = pitch_type)) + Static_Poster_Theme +
+  labs(title = "Correlation Between Pitch Speed, Exit Velocity",
        subtitle = "R-squared = .8025, slope = .15, p-value = .006",  x = "Pitch Speed (MPH)",
        y = "Exit Velocity (MPH)")
 ggsave("pitch_speed_vs_exit_velo.png", dpi = 300)
@@ -76,35 +86,37 @@ pitch_diff <- pitch_data[,c("pitch_type", "release_speed", "effective_speed")]
 pitch_diff <- pitch_diff[pitch_diff$release_speed!="null" & 
                            pitch_diff$pitch_type!="null" & pitch_diff$effective_speed!="null",]
 pitch_diff <- pitch_diff %>% group_by(pitch_type) %>% 
-  summarise(pitch_speed = mean(release_speed, na.rm=TRUE), effective_speed = mean(effective_speed),
+  summarise(pitch_speed = mean(release_speed, na.rm=TRUE), effective_speed = mean(effective_speed, na.rm = TRUE),
             diff = mean(effective_speed - release_speed), num_pitches = n())
 View(pitch_diff)
-ggplot(pitch_diff, aes(x = pitch_type, y = diff)) + geom_bar(stat = "identity") + Static_Poster_Theme +
-  labs(title = "How Pitch Type Effects Perceived Speed", subtitle = "Effective speed - real speed for each pitch", 
+ggplot(pitch_diff, aes(x = pitch_type, y = diff, fill = pitch_type)) + geom_bar(stat = "identity") +
+  Static_Poster_Theme + scale_fill_brewer(type = "qual", palette = "Dark2") +
+  labs(title = "Pitches Appear Slower to Hitters", subtitle = "This effect is magnified for off-speed pitches", 
        x = "Pitch Type", y = "Effective Speed - Real Speed (MPH)")
 ggsave("speed_effect.png", dpi = 300)
 # conclusion: All pitches are slower, but off speed pitches tend to look slower comparatively
 
 ## spin rate vs horizontal/vertical movement for different pitches
-ggplot(pd_right, aes(x = as.numeric(as.character(release_spin_rate)),
-                       y = as.numeric(as.character(pfx_x)), color = pitch_type)) +
-  geom_point(alpha = .5) + geom_smooth(method = "lm") + Static_Poster_Theme +
-  labs(title = "Spin Rate vs Horizontal Movement for Righties",
-       subtitle = "Relationship between spin rate and horizontal movement for each pitch type",
+ggplot(sample_n(pd_right, 1500), aes(x = release_spin_rate,
+                       y = pfx_x, color = pitch_type)) +
+  geom_point(alpha = .5) + geom_smooth(method = "lm", se = FALSE) + Static_Poster_Theme +
+  scale_color_brewer(type = "qual", palette = "Dark2") +
+  labs(title = "More Spin Makes Off-Speed Pitches Break More",
+       subtitle = "Fastballs don't appear to have this same property",
        x = "Spin Rate (RPM)", y = "Horizontal Movement (inches)", color = "Pitch Type")
 ggsave("srate_vs_horizmovement.png", dpi = 300)
-ggplot(pd_right, aes(x = as.numeric(as.character(release_spin_rate)),
-                     y = as.numeric(as.character(pfx_z)), color = pitch_type)) +
+ggplot(sample_n(pd_right, 1000), aes(x = release_spin_rate,
+                     y = pfx_z, color = pitch_type)) +
   geom_point() + geom_smooth(method = "lm") +
   labs(title = "Vertical Movement of Pitches vs Spin Rate (righties)",
        x = "Spin Rate (RPM)", y = "Vertical Movement (inches)", color = "Pitch Type")
 
 #Horizontal vs vertical by pitch type
-ggplot(pd_right, aes(x = as.numeric(as.character(pfx_x)),
+ggplot(sample_n(pd_right, 1000), aes(x = as.numeric(as.character(pfx_x)),
                      y = as.numeric(as.character(pfx_z)), color = pitch_type)) +
   geom_point(alpha = .5) + Static_Poster_Theme +
-  labs(title = "Pitch Movement for Right Handed Pitchers",
-       subtitle = "Horizontal and vertical movement for each pitch type",
+  labs(title = "Similar Pitch Types Move in Similar Ways",
+       subtitle = "Fastballs break up and in, off-speed pitches break down and out",
        x = "Horizontal Movement (inches)", y = "Vertical Movement (inches)", color = "Pitch Type")
 ggsave("vert_horiz_movement.png", dpi = 300)
 
